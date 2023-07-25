55443f5ea7af61b896f7202a3be93d5df9794457
TRIOLI LOW <trioli.low@gmail.com>
2023-07-25 23:34:36 +0300

    init

---
+++ 1_home_work/1_home_work.cpp
@@ -0,0 +1,51 @@
+﻿#include <thread>
+#include <iostream>
+#include <future>
+#include <vector>
+
+
+void async_find(const std::vector<int>& arr, int start, int end, std::promise<int>& prom) {
+    int min = start;
+    for (int i = start + 1; i < end; ++i) {
+        if (arr[i] < arr[min]) {
+            min = i;
+        }
+    }
+    prom.set_value(min);
+}
+
+void sort_arr(std::vector<int>& arr) {
+    int size = arr.size();
+    for (int i = 0; i < size; ++i) {
+        std::promise<int> prom;
+        std::future<void> fut = std::async(async_find, arr, i, size, std::ref(prom));;
+        
+        int min = prom.get_future().get();
+
+        if (min != i) {
+
+            std::swap(arr[i], arr[min]);
+        }
+
+    }
+}
+
+
+int main()
+{
+    std::vector<int> arr = { 64, 25, 12, 22, 11, 87, 152, 24, 65, 39, 21, 14 };
+    std::cout << "Original array: ";
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+
+   sort_arr(arr);
+
+    std::cout << "\nSorted array: ";
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ 2_home_work/2_home_work.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <future>
+#include <thread>
+#include <iostream>
+
+template <typename It, typename Func>
+void main_process(It begin, It end, Func func) {
+    std::for_each(begin, end, func);
+}
+
+template <typename It, typename Func>
+void async_process(It first, It last, Func func) {
+    const auto size = std::distance(first, last);
+    const auto part = 10;// пороговое значение
+    if (size <= part) {
+        main_process (first, last, func);
+    }
+    else {
+        It mid = std::next(first, size / 2);
+        std::shared_future<void> future = std::async(std::launch::async, async_process<It, Func>, first, mid, func);
+
+        async_process(mid, last, func);
+        
+        future.get();
+    }
+}
+
+
+int main()
+{
+    std::vector<int> numbers(1000, 5);
+
+    async_process(numbers.begin(), numbers.end(), [](int num) {
+        std::cout << num << " ";
+        });
+
+}
